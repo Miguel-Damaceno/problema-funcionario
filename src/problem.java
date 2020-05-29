4caf491fb32e65b817e6fb97abe0f069016b4606
Miguel Damaceno <miguel.araujo669@gmail.com>
2020-05-29 19:38:33 -0300

    Menu repetindo

--- src/problem.java
+++ src/problem.java
@@ -35,6 +35,27 @@ public class problem {
 		System.out.printf("Digitar outro (S/N)? ");
 		digitar = sc.next().charAt(0);
 		
+		/* Impressão do menu */
+		System.out.println("");
+		System.out.println("MENU");
+		System.out.println("1 - Total de horas trabalhadas");
+		System.out.println("2 - Custo total");
+		System.out.println("3 - Nome da pessoa que ganhou mais");
+		System.out.println("4 - Sair");
+		System.out.println("Digite uma opção: ");
+		
+		int opcao;
+		opcao = sc.nextInt();
+		
+		while (opcao != 4) {
+			System.out.println("MENU");
+			System.out.println("1 - Total de horas trabalhadas");
+			System.out.println("2 - Custo total");
+			System.out.println("3 - Nome da pessoa que ganhou mais");
+			System.out.println("4 - Sair");
+			System.out.println("Digite uma opção: ");
+			opcao = sc.nextInt();
+		}
 		
 	
 		
