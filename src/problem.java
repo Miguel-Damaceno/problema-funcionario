1019496133796734a49016fede184d24387b286d
Miguel Damaceno <miguel.araujo669@gmail.com>
2020-05-29 19:28:14 -0300

    Leitura dos dados

--- src/problem.java
+++ src/problem.java
@@ -7,10 +7,36 @@ public class problem {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		String nome;
+		double horatrab, valorhora;
+		char digitar;
+		
+		System.out.printf("Nome: ");
+		nome = sc.next();
+		
+		System.out.printf("Horas trabalhadas: ");
+		horatrab = sc.nextDouble();
+		
+		while (horatrab < 0) {
+			System.out.println("Valor invalido");
+			System.out.printf("Horas trabalhadas: ");
+			horatrab = sc.nextDouble();
+		}
+		
+		System.out.printf("Valor por hora: ");
+		valorhora = sc.nextDouble();
+		
+		while (valorhora < 0) {
+			System.out.println("Valor invalido");
+			System.out.printf("Valor por hora: ");
+			valorhora = sc.nextDouble();
+		}
+		
+		System.out.printf("Digitar outro (S/N)? ");
+		digitar = sc.next().charAt(0);
 		
 		
-		
-		
+	
 		
 		sc.close();
 	}
